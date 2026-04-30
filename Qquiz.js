acb430acf87b697e8059cd8f0494adee053ed3ce
edugrowth5-rgb <edugrowth5@gmail.com>
2026-04-30 18:18:41 +0530

    Create Qquiz.js

---
+++ Qquiz.js
@@ -0,0 +1,9 @@
+const qQuizSessions = {
+    1: [
+        { q: "What is the capital of Bihar?", a: "Patna" },
+        { q: "Who was the first person to walk in space?", a: "Alexei Leonov" },
+        { q: "Which bird builds its nest in a burrow?", a: "Puffin" },
+        { q: "What is the technical term for Spacewalk?", a: "EVA" },
+        { q: "Which tree is found only in Mauritius?", a: "Bois Dentelle" }
+    ]
+};
